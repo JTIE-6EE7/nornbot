2033acb8dc7816cbd24c1ee9412c4b6a09f193c0
JT <foosyou@gmail.com>
2024-08-25 11:46:47 -0500

    refactor

---
+++ nr_init.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# nr_init.py
+
+import logging
+import os
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+
+LOGGER = logging.getLogger(__name__)
+
+def nr_init():
+
+    # Init the Norn!
+    nr = InitNornir(
+        inventory={
+            "plugin": "NautobotInventory",
+            "options": {
+                "nautobot_url": os.getenv("NAUTOBOT_URL"),
+                "nautobot_token": os.getenv("NAUTBOT_TOKEN"),
+                "filter_parameters": {"location": "Lab_A"},
+                "ssl_verify": False,
+            },
+        },
+    )
+
+    # get creds from env variables if not set in inventory
+    if nr.inventory.defaults.username == None:
+        nr.inventory.defaults.username = os.getenv("NORNIR_USERNAME")
+
+    if nr.inventory.defaults.password == None:
+        nr.inventory.defaults.password = os.getenv("NORNIR_PASSWORD")
+
+    return nr
+
+if __name__ == "__main__":
+    nr_init()
